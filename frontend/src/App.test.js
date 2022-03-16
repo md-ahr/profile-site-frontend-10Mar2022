93ffa6d0337665ca882ffe05a1e11929d0e1e541
Abdul Halim <ahr.web.pro@gmail.com>
2022-03-16 23:44:23 +0600

    Add test script file and install cloudinary package

---
+++ frontend/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('should render login page', async () => {
+    render(<App />);
+    const element = screen.getByText(/login/i);
+    expect(element).toBeInTheDocument();
+});
