1c01c17ec2084940b9dc7c53b000a29e1403c385
Abdul Halim <halim@rokomari.com>
2023-06-22 11:16:13 +0600

    Update proxy url

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -23,7 +23,7 @@ const Login = () => {
 
   const userLogin = async() => {
     try {
-      const res: AxiosResponse<any> = await axios.post('/api/v1/auth/login', user);
+      const res: AxiosResponse<any> = await axios.post('/auth/login', user);
       if (res.status === 200) {
         toast.success(res.data.message);
         localStorage.setItem('token', res.data.token);
